cc7be21e3dc974fa644ba8a8be188f1b9a321dc2
Frank Pei <peistare@gmail.com>
2016-09-23 21:33:59 +0800

    first commit

---
+++ src/main/java/org/example/demo1/JavaCode1.java
@@ -0,0 +1,8 @@
+package org.example.demo1;
+
+public class JavaCode1 {
+	public static void main(String[] args) {
+		System.out.println("My First Java Code1");
+		System.out.println("Welcome my TeamMate FLZ!");
+	}
+}
