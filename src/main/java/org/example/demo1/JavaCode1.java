8ee458efb571077aaa289433bc48dd9835e195a4
FLZhen <FLZhen@FLzhen>
2016-09-23 22:58:09 +0800

    FLZ commit

--- src/main/java/org/example/demo1/JavaCode1.java
+++ src/main/java/org/example/demo1/JavaCode1.java
@@ -4,5 +4,7 @@ public class JavaCode1 {
 	public static void main(String[] args) {
 		System.out.println("My First Java Code1");
 		System.out.println("Welcome my TeamMate FLZ!");
+		
+		System.out.println("my code");
 	}
 }
